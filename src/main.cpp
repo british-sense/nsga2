f8e657cf9eab838058ea3789d611b8362aaa0c1d
ko-cha <xtej3g@gmail.com>
2020-01-21 17:40:12 +0900

    ✌

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,19 @@
 #include "nsga2.hpp"
 #include "utils.hpp"
 
-void repeated_permutation(const int start, const int end, std::vector<int> & v) {
+void repeated_permutation(int pos, std::vector<int> & v, const int size, const int start, const int end) {
 
-    if(v.size() == param::dimension) {
+    if(pos == size) {
+        for(int d = 0; d < param::dimension; d++) param::domain.at(d) = std::make_pair(0.0, 0.0);
         std::vector<Individual> population = nsga2();
         std::string filename = "";
         param::output_filename = filename;
-        output_file(population);
-        v.clear();
+        // output_file(population);
         return;
     }
-    for(int i = start; i <= end; i++) {
-        v.emplace_back(i);
-        repeated_permutation(start, end, v);
+    for(int i = start; i < end; i++) {
+        v.at(pos) = i;
+        repeated_permutation(pos + 1, v, size, start, end);
     }
 }
 
@@ -28,7 +28,10 @@ int main(int argc, char * argv[]) {
     params_setting(argc, argv);
     param::output_parameter();
 
-    std::vector<Individual> population = nsga2();
+    // std::vector<Individual> population = nsga2();
 
-    output_file(population);
+    // output_file(population);
+
+    std::vector<int> v(param::dimension);
+    repeated_permutation(0, v, param::dimension, 0, param::partition);
 }
